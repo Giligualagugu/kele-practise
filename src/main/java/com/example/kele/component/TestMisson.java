56e9249ad1365a8cba45c3648e05f6569b123825
xukele <1421295023@qq.com>
2019-06-04 16:02:17 +0800

    引入定时任务

---
+++ src/main/java/com/example/kele/component/TestMisson.java
@@ -0,0 +1,24 @@
+package com.example.kele.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.time.LocalDate;
+
+/**
+ * @author xukele
+ * @TIME 2019/6/4 15:39
+ * kele-practise & com.example.kele.component
+ */
+@Slf4j
+public class TestMisson extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+
+        log.info("===> test 定时任务,当前时间:{}", LocalDate.now());
+
+    }
+}
---
+++ src/main/java/com/example/kele/config/QuartzConfig.java
@@ -0,0 +1,50 @@
+package com.example.kele.config;
+
+import com.example.kele.component.TestMisson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+/**
+ * @author xukele
+ * @TIME 2019/6/4 15:30
+ * kele-practise & com.example.kele.config
+ */
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetailFactoryBean mission1() {
+
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+
+        jobDetailFactoryBean.setJobClass(TestMisson.class);
+
+        return jobDetailFactoryBean;
+    }
+
+    @Bean
+    public CronTriggerFactoryBean trigger1() {
+        CronTriggerFactoryBean triggerFactoryBean = new CronTriggerFactoryBean();
+
+        triggerFactoryBean.setJobDetail(mission1().getObject());
+
+        triggerFactoryBean.setName("test-mission1");
+        triggerFactoryBean.setGroup("group1");
+
+        triggerFactoryBean.setCronExpression("0/20 * * * * ?");
+        return triggerFactoryBean;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+
+        SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
+        factoryBean.setStartupDelay(10);
+        factoryBean.setTriggers(trigger1().getObject());
+
+        return factoryBean;
+    }
+}
