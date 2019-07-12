d6c583c6dd9dc1ef671ee2661abaa0ffafab8c4a
xukele <1421295023@qq.com>
2019-07-12 18:12:20 +0800

    jenkins 部署

--- src/test/java/com/example/kele/KelePractiseApplicationTests.java
+++ src/test/java/com/example/kele/KelePractiseApplicationTests.java
@@ -2,15 +2,25 @@ package com.example.kele;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KelePractiseApplicationTests {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Test
 	public void contextLoads() {
+
+		Class<? extends DataSource> aClass = dataSource.getClass();
+
+		System.out.println(aClass);
 	}
 
 }
