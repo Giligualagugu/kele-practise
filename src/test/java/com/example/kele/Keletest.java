2a2fcda33d4496251d8b79778de86d9c6e030b82
xukele <1421295023@qq.com>
2019-10-16 11:08:40 +0800

    加密了配置文件密码

--- src/test/java/com/example/kele/Keletest.java
+++ src/test/java/com/example/kele/Keletest.java
@@ -1,17 +1,13 @@
 package com.example.kele;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.UUID;
 
 public class Keletest {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        String s = UUID.randomUUID().toString().replaceAll("-", "");
 
-        String kele123 = passwordEncoder.encode("kele123");
-
-        System.out.println(kele123);
-
+        System.out.println(s);
     }
 }
